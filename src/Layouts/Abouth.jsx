7d2056569ccfff523dd11450a54d7ccbedbac731
H8YOU212 <slava.mikerin@mail.ru>
2024-04-21 23:37:23 +0500

    add store

---
+++ src/Layouts/Abouth.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Abouth = () => {
+  return (
+    <div>About</div>
+  )
+}
+
+export default Abouth
---
+++ src/components/Store/Store.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../Layouts/Header.jsx'
+
+const Store = () => {
+  return (
+    <div>
+        <Header />
+        <div className='see-more'>See more</div>
+    </div>
+  )
+}
+
+export default Store
